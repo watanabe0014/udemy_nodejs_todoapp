e8e82f045c96c070aa62a3be10f1274478b157e9
nell_aim <abiban0014@gmail.com>
2023-01-15 16:42:26 +0900

    mod mongodb

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use("/api/v1/tasks", taskRoute);
 //DBと接続
 const start = async () => {
     try{
-        await connectDB(process.env.DEV_MONGO_URL || process.env.MONGODB_URI);
+        await connectDB(process.env.HEROKU_MONGO_URL || process.env.MONGO_URL);
         app.listen(process.env.PORT || PORT, () => {
             console.log("mongo_todo_appのサーバーが起動しました");
         });
